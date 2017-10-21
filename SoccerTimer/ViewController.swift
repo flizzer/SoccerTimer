262f7e3e40588594f95873fc8d65c94ac5ba0835
flizzer <bdavis@macpro.local>
2017-10-20 20:22:50 -0400

    Initial Commit

---
+++ SoccerTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoccerTimer
+//
+//  Created by Brian Davis on 10/20/17.
+//  Copyright © 2017 Brian Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
