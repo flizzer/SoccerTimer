53e7a58c5d6203dcb1b99528eee8c24e282ddbd7
flizzer <bdavis@macpro.local>
2017-11-19 17:30:10 -0500

    finishing logic for the timer start and countdown

--- SoccerTimer/ViewController.swift
+++ SoccerTimer/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timerDisplay: UILabel!
+    
+    var date = Date()
+    var dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        dateFormatter.dateFormat = "mm:ss"
+        date = dateFormatter.date(from: "08:00")!
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startButton(_ sender: UIButton)
+    {
+        runTimer()
+    }
+    
+    func runTimer()
+    {
+        Timer.scheduledTimer(timeInterval: 1, target: self,
+                                     selector: (#selector(ViewController.updateTimerDisplay)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimerDisplay()
+    {
+        date.addTimeInterval(-1)
+        let updatedTime = dateFormatter.string(from: date)
+        timerDisplay.text = updatedTime
+    }
 }
 
